b0fb7ad174c970a2500529e4333662ce583e8006
Vladyslav Stovmanenko <flaviusglamfenix@gmail.com>
2019-06-05 22:16:28 +0300

    Adjusting unit tests template according to current unit testing model.

--- unitTest/src/main.c
+++ unitTest/src/main.c
@@ -3,10 +3,45 @@
 #include "stddef.h"
 #include "setjmp.h"
 #include "stdint.h"
+#include "stdlib.h"
+#include "string.h"
 
 #include "cmocka.h"
 
-static void
+char** tests;
+
+char** 
+get_available_tests(void)
+{
+	const int NumOfTests = 1;
+	tests = (char**)malloc(sizeof(char*) * NumOfTests + 1);
+
+	tests[0] = (char*)malloc(sizeof(char)*128);
+	strcpy(tests[0], "my_test");
+	
+	tests[1] = NULL;
+
+	return tests;
+}
+
+void
+clean_up()
+{
+	for(int i = 0; tests[i]; i++)
+	{
+		free(tests[i]);
+	}
+
+	free(tests);
+}
+
+void**
+get_states_for_test(void* key)
+{
+
+}
+
+void
 my_test(void** state)
 {
 	printf("%s\n", "Hello, world!");
